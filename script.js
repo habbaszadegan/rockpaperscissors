07d07b088d3e0e346434500ca87762de3efe1ac6
Hossein Abbaszadegan <habbaszadegan@gmail.com>
2023-11-16 11:00:54 +0330

    PROJECT FINISHED
    implemented if statement announcing winner of the game.

--- script.js
+++ script.js
@@ -183,16 +183,15 @@ function game () {
     }
 
     console.log (fifthRound());
+
+    if (pScore > cScore) {
+        console.log(`Player wins ${pScore} to ${cScore}! Woop Woop!`);
+    } else if (pScore < cScore) {
+        console.log(`Computer wins ${cScore} to ${pScore}. Womp Womp.`);
+    } else {
+        console.log(`It's a tie. Play again?`);
+    }
 }
 
 
 console.log (game());
-
-// Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game 
-    // that keeps score and reports a winner or loser at the end.
-//Just call your playRound function 5 times in a row.
-// At this point you should be using console.log() to display the results of each round and the winner at the end.
-// Use prompt() to get input from the user. Read the docs here if you need to.
-// Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return 
-    // value to something more useful.
-// Feel free to create more “helper” functions if you think it would be useful.
